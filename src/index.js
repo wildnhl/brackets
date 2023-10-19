8384b7392e8b54ec2993bd337c4b1ac1d5e71814
wildnhl <minnesotanhlwild@gmail.com>
2023-10-19 21:59:38 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [str[0]]
+  for (let i = 1; i < str.length; i++) {
+    stack.push(str[i])
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (
+        stack[stack.length - 1] == bracketsConfig[j][1] &&
+        stack[stack.length - 2] == bracketsConfig[j][0]
+      ) {
+        stack.pop()
+        stack.pop()
+      }
+    }
+  }
+  return stack.length == 0
 }
